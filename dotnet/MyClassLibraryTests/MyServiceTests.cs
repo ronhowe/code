59266c783f8eeba4c55c65046c8b87205ce119db
Ron Howe <ronhowe@hotmail.com>
2024-12-05 12:49:30 -0500

    Refactored MyService Tests

--- dotnet/MyClassLibraryTests/MyServiceTests.cs
+++ dotnet/MyClassLibraryTests/MyServiceTests.cs
@@ -6,13 +6,34 @@ using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 using MyClassLibrary;
+using Serilog;
+using Serilog.Events;
 using System.Diagnostics;
 
 namespace MyClassLibraryTests;
 
 [TestClass]
-public sealed class MyServiceTests : TestBase
+public sealed class MyServiceTests
 {
+    internal readonly string _outputTemplate = "[{Timestamp:yyyy-MM-dd @ HH:mm:ss.fff}] [{Level:u3}] [{SourceContext}] [{MachineName}] {Message}{NewLine}{Exception}";
+    internal readonly string _sourceContext = nameof(MyServiceTests);
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        Trace.WriteLine("Power-On Self-Test");
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .WriteTo.Console(outputTemplate: _outputTemplate)
+            .CreateLogger();
+
+        Log.ForContext("SourceContext", _sourceContext).Debug("Initializing Test");
+    }
+
     [TestMethod]
     public void MyMethodLogsEntryMessage()
     {
@@ -65,6 +86,5 @@ public sealed class MyServiceTests : TestBase
     [TestMethod]
     public void PowerOnSelfTest()
     {
-        Trace.WriteLine("Power-On Self-Test");
     }
 }
