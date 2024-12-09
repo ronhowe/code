ecd041cd9e4c30181a8b3ad6e9f61fcb6e7859f6
Ron Howe <ronhowe@hotmail.com>
2024-12-09 18:32:33 -0500

    Added MyConsoleApp

---
+++ dotnet/MyConsoleApp/Program.cs
@@ -0,0 +1,68 @@
+﻿/*******************************************************************************
+https://github.com/ronhowe
+*******************************************************************************/
+
+using FluentAssertions;
+using System.Diagnostics;
+using System.Net;
+
+namespace ConsoleApp1;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Uri uri = new(args[0]);
+
+        var background = Console.BackgroundColor;
+        Console.CancelKeyPress += (sender, e) =>
+        {
+            Console.BackgroundColor = background;
+            Console.Clear();
+        };
+
+        Stopwatch stopwatch = new();
+
+        while (true)
+        {
+            HttpClient client = new();
+
+            stopwatch.Start();
+
+            try
+            {
+                var response = client.GetAsync(uri).Result;
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+                Refresh("OK", stopwatch.ElapsedMilliseconds, uri, ConsoleColor.DarkGreen);
+
+                foreach (var header in response.Headers)
+                {
+                    if (header.Key == "CustomHeader")
+                    {
+                        Console.WriteLine($"{header.Key} = {header.Value.FirstOrDefault<string>()}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Refresh(e.Message, stopwatch.ElapsedMilliseconds, uri, ConsoleColor.DarkRed);
+            }
+            finally
+            {
+                stopwatch.Stop();
+                stopwatch.Reset();
+                client.Dispose();
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+
+    private static void Refresh(string message, long duration, Uri uri, ConsoleColor color)
+    {
+        Console.BackgroundColor = color;
+        Console.Clear();
+        Console.WriteLine($"{DateTime.UtcNow} - {uri} - {message} - {duration} ms");
+    }
+}
