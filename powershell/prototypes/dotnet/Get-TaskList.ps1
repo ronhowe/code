e2f155a8a5c278f7100f748dace89619ec083274
Ron Howe <ronhowe@hotmail.com>
2024-12-16 11:06:27 -0500

    Style

--- powershell/prototypes/dotnet/Get-TaskList.ps1
+++ powershell/prototypes/dotnet/Get-TaskList.ps1
@@ -7,13 +7,15 @@ param (
     [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
     [ValidateScript({ Test-Path -Path $_ })]
-    [string]$Path = "$HOME\repos\ronhowe\code\dotnet",
+    [string]$Path = "$HOME\repos\ronhowe\code",
 
     [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
     [string]$Pattern = "(LINK|NOTE|TODO)"
 )
-Get-ChildItem -Path $Path -Include @("*.cs", "*.json") -Recurse |
+# TODO: Refactor -Include as input parameter with a default.
+# TODO: Add tasks alias to profile.
+Get-ChildItem -Path $Path -Include @("*.bicep", "*.cs", "*.json", "*.ps*", "*.sql", "*.txt", "*.xml") -Recurse |
 Where-Object { $_.FullName -notlike "*\bin\*" -and $_.FullName -notlike "*\obj\*" } |
 Sort-Object -Property "FullName" |
 Select-String -Pattern $Pattern -CaseSensitive |
