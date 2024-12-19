d2b29960c05b48bcf414d191925d931bcc572fe9
Ron Howe <ronhowe@hotmail.com>
2024-12-19 15:06:49 -0500

    Improved Clear-LocalStorage

--- powershell/prototypes/dotnet/Clear-LocalStorage.ps1
+++ powershell/prototypes/dotnet/Clear-LocalStorage.ps1
@@ -1,17 +1,19 @@
 #requires -Module "Az.Storage"
 #requires -Module "SqlServer"
 
-$ErrorActionPreference = "Stop"
-Write-Output "Running $($MyInvocation.MyCommand.Name)"
+[CmdletBinding()]
+param()
 
-Write-Output "Truncating Database Table"
+$ErrorActionPreference = "Stop"
+
+Write-Verbose "Truncating Database Table"
 Invoke-SqlCmd -ConnectionString "Server=LOCALHOST;Database=MyDatabase;Integrated Security=True;Application Name=$($MyInvocation.MyCommand.Name);Encrypt=False;Connect Timeout=1;Command Timeout=0;" -Query "TRUNCATE TABLE [MyTable];"
 
-Write-Output "Removing Azure Storage Table"
+Write-Verbose "Removing Azure Storage Table"
 New-AzStorageContext -ConnectionString "UseDevelopmentStorage=true;" |
 Get-AzStorageTable -Name "MyCloudTable" -ErrorAction SilentlyContinue |
 Remove-AzStorageTable -Name "MyCloudTable" -Force
 
-Write-Output "Removing Logs"
+Write-Verbose "Removing Logs"
 Get-ChildItem -Path "$HOME\repos\ronhowe\code\logs" -Recurse |
 Remove-Item -Force
