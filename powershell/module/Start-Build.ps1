25be196db28eedf69586df2dcd511c165b0ccc60
Ron Howe <ronhowe@hotmail.com>
2024-12-20 10:40:53 -0500

    Fixed PowerShell Module Start Build Script

--- powershell/module/Start-Build.ps1
+++ powershell/module/Start-Build.ps1
@@ -22,7 +22,7 @@ $parameters = @{
         "$PSScriptRoot\..\dependencies\Dependencies.psd1",
         "$PSScriptRoot\..\dependencies\Dependencies.Tests.ps1",
         "$PSScriptRoot\..\dependencies\Install-Dependencies.ps1",
-        "$PSScriptRoot\..\dependencies\Invoke-Dependencies.Tests.ps1",
+        "$PSScriptRoot\..\dependencies\Invoke-PesterTests.ps1",
         "$PSScriptRoot\LICENSE*"
         # "$PSScriptRoot\README.md"
     )
