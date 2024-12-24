dd9745932615a57fda764041a2d45bb7198b5638
Ron Howe <ronhowe@hotmail.com>
2024-12-24 09:48:52 -0500

    Added PowerShell Template

---
+++ powershell/runbooks/New-Template.ps1
@@ -0,0 +1,16 @@
+[CmdletBinding()]
+param(
+)
+begin {
+    Write-Verbose "Beginning $($MyInvocation.MyCommand.Name)"
+
+    Get-Variable -Scope "Local" -Include @($MyInvocation.MyCommand.Parameters.Keys) |
+    Select-Object -Property @("Name", "Value") |
+    ForEach-Object { Write-Debug "`$$($_.Name) = $($_.Value)" }
+}
+process {
+    Write-Verbose "Processing $($MyInvocation.MyCommand.Name)"
+}
+end {
+    Write-Verbose "Ending $($MyInvocation.MyCommand.Name)"
+}
