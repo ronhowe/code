06e1e73b992dfc7c990b409e0f552159878be0f3
Ron Howe <ronhowe@hotmail.com>
2024-12-26 15:04:32 -0500

    Updated PowerShell Profile

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -15,40 +15,42 @@ process {
 
     $ErrorActionPreference = "Stop" # changed from Continue
     $ProgressPreference = "SilentlyContinue" # changed from Continue
-    $VerbosePreference = "SilentlyContinue" # unchanged
-    $WarningPreference = "Continue" # unchanged
 
     Write-Verbose "Asserting PowerShell Core"
     if ($PSVersionTable.PSEdition -ne "Core") {
-        Write-Warning "PowerShell Core Not Detected" -WarningAction Continue
+        Write-Warning "PowerShell Core Not Detected"
     }
 
-    Write-Verbose "Importing Az.Tools.Predictor"
+    Write-Verbose "Asserting Az.Tools.Predictor Module Exists"
     if (Get-Module -Name "Az.Tools.Predictor" -ListAvailable) {
+        Write-Verbose "Importing Az.Tools.Predictor"
         Import-Module -Name "Az.Tools.Predictor" -Verbose:$false
     }
     else {
         Write-Warning "Skipping Az.Tools.Predictor"
     }
 
-    Write-Verbose "Importing Az.Tools.Predictor"
+    Write-Verbose "Asserting Microsoft.PowerShell.SecretManagement Module Exists"
     if (Get-Module -Name "Microsoft.PowerShell.SecretManagement" -ListAvailable) {
+        Write-Verbose "Importing Az.Tools.Predictor"
         Import-Module -Name "Microsoft.PowerShell.SecretManagement" -Verbose:$false
     }
     else {
         Write-Warning "Skipping Microsoft.PowerShell.SecretManagement"
     }
 
-    Write-Verbose "Importing Az.Tools.Predictor"
+    Write-Verbose "Asserting Microsoft.PowerShell.SecretStore Module Exists"
     if (Get-Module -Name "Microsoft.PowerShell.SecretStore" -ListAvailable) {
+        Write-Verbose "Importing Az.Tools.Predictor"
         Import-Module -Name "Microsoft.PowerShell.SecretStore" -Verbose:$false
     }
     else {
         Write-Warning "Skipping Microsoft.PowerShell.SecretStore"
     }
 
-    Write-Verbose "Importing Az.Tools.Predictor"
+    Write-Verbose "Asserting posh-git Module Exists"
     if (Get-Module -Name "posh-git" -ListAvailable) {
+        Write-Verbose "Importing Az.Tools.Predictor"
         Import-Module -Name "posh-git" -Verbose:$false
     }
     else {
@@ -56,9 +58,11 @@ process {
     }
 
     ## NOTE: Work shim.
+    Write-Verbose "Asserting Windows PowerShell ISE Host"
     if ($host.Name -eq "Windows PowerShell ISE Host") {
-        Write-Verbose "Importing Az.Tools.Predictor"
+        Write-Verbose "Asserting ISESteroids Module Exists"
         if (Get-Module -Name "ISESteroids" -ListAvailable) {
+            Write-Verbose "Importing ISESteroids"
             Import-Module -Name "ISESteroids" -Verbose:$false
         }
         else {
@@ -78,20 +82,26 @@ process {
         Set-PSReadLineOption -PredictionViewStyle InlineView -WarningAction SilentlyContinue
     }
 
-    Write-Verbose "Asserting Shell Module Exists"
-    if (Test-Path -Path "$HOME\repos\ronhowe\code\powershell\module\Output\Shell\Shell.psm1") {
-        Write-Verbose "Removing Shell Module"
-        Get-Module -Name "Shell" |
-        Remove-Module -Force
+    Write-Verbose "Asserting PowerShell Core"
+    if ($PSVersionTable.PSEdition -eq "Core") {
+        Write-Verbose "Asserting Shell Module Exists"
+        if (Test-Path -Path "$HOME\repos\ronhowe\code\powershell\module\Output\Shell\Shell.psm1") {
+            Write-Verbose "Removing Shell Module"
+            Get-Module -Name "Shell" |
+            Remove-Module -Force
 
-        Write-Verbose "Importing Shell Module"
-        Import-Module -Name "$HOME\repos\ronhowe\code\powershell\module\Output\Shell" -Force
+            Write-Verbose "Importing Shell Module"
+            Import-Module -Name "$HOME\repos\ronhowe\code\powershell\module\Output\Shell" -Force
     
-        Write-Verbose "Starting Shell"
-        Start-Shell
+            Write-Verbose "Starting Shell"
+            Start-Shell
+        }
+        else {
+            Write-Warning "Shell Module Not Found"
+        }
     }
     else {
-        Write-Warning "Shell Module Not Found"
+        Write-Warning "Skipping Shell"
     }
 }
 end {
