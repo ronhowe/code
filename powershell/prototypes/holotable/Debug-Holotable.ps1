8cb959031e7ad5b4f841fcaec49de6df6c24a32c
Ron Howe <ronhowe@hotmail.com>
2024-12-27 14:53:04 -0500

    Updated Holotable Debug Script

--- powershell/prototypes/holotable/Debug-Holotable.ps1
+++ powershell/prototypes/holotable/Debug-Holotable.ps1
@@ -1,103 +1,87 @@
 #requires -PSEdition Core
+[CmdletBinding()]
+param(
+)
+begin {
+    Write-Verbose "Beginning $($MyInvocation.MyCommand.Name)"
 
-#region setup
-
-Clear-Host
-
-Set-Location -Path "~\repos\ronhowe\powershell\holotable"
-
-Import-Module -Name ".\Holotable.psm1" -Force -Verbose
-
-$vscodePath = "C:\Users\ronhowe\AppData\Local\Programs\Microsoft VS Code\Code.exe"
-
-#endregion setup
-
-#region export dark side
-
-Write-Verbose "Exporting Dark Side CDF"
-$parameters = @{
-    JsonPath       = "~\repos\ronhowe\swccg-card-json\Dark.json"
-    JsonLegacyPath = "~\repos\ronhowe\swccg-card-json\DarkLegacy.json"
-    JsonSetsPath   = "~\repos\ronhowe\swccg-card-json\sets.json"
-    CdfPath        = "~\repos\ronhowe\powershell\holotable\darkside.cdf"
-    # IdFilter       = 634 # Darth Vader (Premiere)
-    # SetFilter      = "*17*"
-    # TitleFilter    = "*Darth Vader*"
-    # TypeFilter     = "Objective"
+    Get-Variable -Scope "Local" -Include @($MyInvocation.MyCommand.Parameters.Keys) |
+    Select-Object -Property @("Name", "Value") |
+    ForEach-Object { Write-Debug "`$$($_.Name) = $($_.Value)" }
 }
-Export-Cdf @parameters
+process {
+    Write-Verbose "Processing $($MyInvocation.MyCommand.Name)"
 
-#endregion export dark side
+    Import-Module -Name "$HOME\repos\ronhowe\code\powershell\prototypes\holotable\Holotable.psm1" -Force
 
-#region diff dark side
+    # $vscodePath = "C:\Users\ronhowe\AppData\Local\Programs\Microsoft VS Code\Code.exe"
 
-Write-Verbose "Diffing Dark Side CDF"
-$parameters = @{
-    Path         = $vscodePath
-    ArgumentList = @(
-        "--diff",
-        (Resolve-Path -Path "~\repos\ronhowe\holotable\darkside.cdf"),
-        (Resolve-Path -Path "~\repos\ronhowe\powershell\holotable\darkside.cdf")
-    )
-    NoNewWindow  = $true
-    Wait         = $true
+    Write-Verbose "Exporting Dark Side CDF"
+    $parameters = @{
+        JsonPath       = "$HOME\repos\ronhowe\swccg-card-json\Dark.json"
+        JsonLegacyPath = "$HOME\repos\ronhowe\swccg-card-json\DarkLegacy.json"
+        JsonSetsPath   = "$HOME\repos\ronhowe\swccg-card-json\sets.json"
+        CdfPath        = "$HOME\repos\ronhowe\code\powershell\prototypes\holotable\darkside.cdf"
+        # IdFilter       = 634 # Darth Vader (Premiere)
+        # SetFilter      = "*17*"
+        # TitleFilter    = "*Darth Vader*"
+        # TypeFilter     = "Objective"
+    }
+    Export-Cdf @parameters
+
+    Write-Verbose "Diffing Dark Side CDF"
+    $parameters = @{
+        Path         = "code"
+        ArgumentList = @(
+            "--diff",
+            (Resolve-Path -Path "$HOME\repos\ronhowe\holotable\darkside.cdf"),
+            (Resolve-Path -Path "$HOME\repos\ronhowe\code\powershell\prototypes\holotable\darkside.cdf")
+        )
+        NoNewWindow  = $true
+        Wait         = $true
+    }
+    Start-Process @parameters
+
+    Write-Verbose "Exporting Light Side CDF"
+    $parameters = @{
+        JsonPath       = "$HOME\repos\ronhowe\swccg-card-json\Light.json"
+        JsonLegacyPath = "$HOME\repos\ronhowe\swccg-card-json\LightLegacy.json"
+        JsonSetsPath   = "$HOME\repos\ronhowe\swccg-card-json\sets.json"
+        CdfPath        = "$HOME\repos\ronhowe\code\powershell\prototypes\holotable\Lightside.cdf"
+        # IdFilter       = 1593 # Luke Skywalker (Premiere)
+        # SetFilter      = "*17*"
+        # TitleFilter    = "*Luke Skywalker*"
+        # TypeFilter     = "Objective"
+    }
+    Export-Cdf @parameters
+
+    Write-Verbose "Diffing Light Side CDF"
+    $parameters = @{
+        Path         = "code"
+        ArgumentList = @(
+            "--diff",
+            (Resolve-Path -Path "$HOME\repos\ronhowe\holotable\lightside.cdf"),
+            (Resolve-Path -Path "$HOME\repos\ronhowe\code\powershell\prototypes\holotable\lightside.cdf")
+        )
+        NoNewWindow  = $true
+        Wait         = $true
+    }
+    Start-Process @parameters
+
+    Write-Verbose "Getting Dark Side Statistics..."
+    Get-Content -Path "$HOME\repos\ronhowe\swccg-card-json\Dark.json" |
+    ConvertFrom-Json |
+    Select-Object -ExpandProperty "cards" |
+    Measure-Object -Property id -Minimum -Maximum |
+    Select-Object -Property @{Name = "Side"; Expression = { "Dark" } }, @{Name = "Total Cards"; Expression = { $_.Count } }, @{Name = "Minimum Id"; Expression = { $_.Minimum } }, @{Name = "Maximum Id"; Expression = { $_.Maximum } }
+
+    Write-Verbose "Getting Light Side Statistics..."
+    Get-Content -Path "$HOME\repos\ronhowe\swccg-card-json\Light.json" |
+    ConvertFrom-Json |
+    Select-Object -ExpandProperty "cards" |
+    Measure-Object -Property id -Minimum -Maximum |
+    Select-Object -Property @{Name = "Side"; Expression = { "Light" } }, @{Name = "Total Cards"; Expression = { $_.Count } }, @{Name = "Minimum Id"; Expression = { $_.Minimum } }, @{Name = "Maximum Id"; Expression = { $_.Maximum } }
 }
-Start-Process @parameters
-
-#endregion diff dark side
-
-#region export light side
-
-Write-Verbose "Exporting Light Side CDF"
-$parameters = @{
-    JsonPath       = "~\repos\ronhowe\swccg-card-json\Light.json"
-    JsonLegacyPath = "~\repos\ronhowe\swccg-card-json\LightLegacy.json"
-    JsonSetsPath   = "~\repos\ronhowe\swccg-card-json\sets.json"
-    CdfPath        = "~\repos\ronhowe\powershell\holotable\lightside.cdf"
-    # IdFilter       = 1593 # Luke Skywalker (Premiere)
-    # SetFilter      = "*17*"
-    # TitleFilter    = "*Luke Skywalker*"
-    # TypeFilter     = "Objective"
+end {
+    Write-Verbose "Ending $($MyInvocation.MyCommand.Name)"
 }
-Export-Cdf @parameters
-
-#endregion export light side
-
-#region diff light side
-
-Write-Verbose "Diffing Light Side CDF"
-$parameters = @{
-    Path         = $vscodePath
-    ArgumentList = @(
-        "--diff",
-        (Resolve-Path -Path "~\repos\ronhowe\holotable\lightside.cdf"),
-        (Resolve-Path -Path "~\repos\ronhowe\powershell\holotable\lightside.cdf")
-    )
-    NoNewWindow  = $true
-    Wait         = $true
-}
-Start-Process @parameters
-
-#endregion diff light side
-
-#region dark side stats
-
-Write-Verbose "Getting Dark Side Statistics..."
-Get-Content -Path "~\repos\ronhowe\swccg-card-json\Dark.json" |
-ConvertFrom-Json |
-Select-Object -ExpandProperty "cards" |
-Measure-Object -Property id -Minimum -Maximum |
-Select-Object -Property @{Name = "Side"; Expression = { "Dark" } }, @{Name = "Total Cards"; Expression = { $_.Count } }, @{Name = "Minimum Id"; Expression = { $_.Minimum } }, @{Name = "Maximum Id"; Expression = { $_.Maximum } }
-
-#endregion dark side stats
-
-#region light side stats
-
-Write-Verbose "Getting Light Side Statistics..."
-Get-Content -Path "~\repos\ronhowe\swccg-card-json\Light.json" |
-ConvertFrom-Json |
-Select-Object -ExpandProperty "cards" |
-Measure-Object -Property id -Minimum -Maximum |
-Select-Object -Property @{Name = "Side"; Expression = { "Light" } }, @{Name = "Total Cards"; Expression = { $_.Count } }, @{Name = "Minimum Id"; Expression = { $_.Minimum } }, @{Name = "Maximum Id"; Expression = { $_.Maximum } }
-
-#endregion light side stats
