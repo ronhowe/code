d668c45bcca7fe9f12be53ed5ce1a8c21323ad64
Ron Howe <ronhowe@hotmail.com>
2024-12-28 08:31:00 -0500

    Updated PowerShell Runbook

--- powershell/runbooks/Show-DevOpsTools.ps1
+++ powershell/runbooks/Show-DevOpsTools.ps1
@@ -14,54 +14,104 @@ process {
     $ErrorActionPreference = "Continue"
 
     Write-Verbose "Getting .NET (dotnet) Version"
-    $(dotnet --version) |
-    Out-String |
-    Write-Host
+    if (Get-Command -Name "dotnet" -ErrorAction SilentlyContinue) {
+        $(dotnet --version) |
+        Out-String |
+        Write-Host
+    }
+    else {
+        Write-Warning ".NET (dotnet) Not Found"
+    }
 
-    Write-Verbose "Getting Azure CLI (az) Version"
-    $(az --version) |
-    Out-String |
-    Write-Host
+    if (Get-Command -Name "az" -ErrorAction SilentlyContinue) {
+        Write-Verbose "Getting Azure CLI (az) Version"
+        $(az --version) |
+        Out-String |
+        Write-Host
+    }
+    else {
+        Write-Warning "Azure CLI (az) Not Found"
+    }
 
-    Write-Verbose "Getting Bicep CLI (bicep) Version"
-    $(bicep --version) |
-    Out-String |
-    Write-Host
+    if (Get-Command -Name "bicep" -ErrorAction SilentlyContinue) {
+        Write-Verbose "Getting Bicep CLI (bicep) Version"
+        $(bicep --version) |
+        Out-String |
+        Write-Host
+    }
+    else {
+        Write-Warning "Bicep CLI (bicep) Not Found"
+    }
 
-    Write-Verbose "Getting Git CLI (git) Version"
-    $(git --version) |
-    Out-String |
-    Write-Host
+    if (Get-Command -Name "git" -ErrorAction SilentlyContinue) {
+        Write-Verbose "Getting Git CLI (git) Version"
+        $(git --version) |
+        Out-String |
+        Write-Host
+    }
+    else {
+        Write-Warning "Git CLI (git) Not Found"
+    }
 
-    Write-Verbose "Getting GitHub CLI (gh) Version"
-    $(gh --version) |
-    Out-String |
-    Write-Host
+    if (Get-Command -Name "gh" -ErrorAction SilentlyContinue) {
+        Write-Verbose "Getting GitHub CLI (gh) Version"
+        $(gh --version) |
+        Out-String |
+        Write-Host
+    }
+    else {
+        Write-Warning "GitHub CLI (gh) Not Found"
+    }
 
-    Write-Verbose "Getting NuGet (nuget) Version"
-    $(nuget | Select-String -SimpleMatch "NuGet Version") |
-    Out-String |
-    Write-Host
+    if (Get-Command -Name "nuget" -ErrorAction SilentlyContinue) {
+        Write-Verbose "Getting NuGet (nuget) Version"
+        $(nuget | Select-String -SimpleMatch "NuGet Version") |
+        Out-String |
+        Write-Host
+    }
+    else {
+        Write-Warning "NuGet (nuget) Not Found"
+    }
 
-    Write-Verbose "Getting PowerShell (pwsh) Version"
-    $(pwsh --version) |
-    Out-String |
-    Write-Host
+    if (Get-Command -Name "pwsh" -ErrorAction SilentlyContinue) {
+        Write-Verbose "Getting PowerShell (pwsh) Version"
+        $(pwsh --version) |
+        Out-String |
+        Write-Host
+    }
+    else {
+        Write-Warning "PowerShell (pwsh) Not Found"
+    }
 
-    Write-Verbose "Getting Python (python) Version"
-    $(python --version) |
-    Out-String |
-    Write-Host
+    if (Get-Command -Name "python" -ErrorAction SilentlyContinue) {
+        Write-Verbose "Getting Python (python) Version"
+        $(python --version) |
+        Out-String |
+        Write-Host
+    }
+    else {
+        Write-Warning "Python (python) Not Found"
+    }
 
-    Write-Verbose "Getting Visual Studio Code (code) Version"
-    $(code --version) |
-    Out-String |
-    Write-Host
+    if (Get-Command -Name "code" -ErrorAction SilentlyContinue) {
+        Write-Verbose "Getting Visual Studio Code (code) Version"
+        $(code --version) |
+        Out-String |
+        Write-Host
+    }
+    else {
+        Write-Warning "Visual Studio Code (code) Not Found"
+    }
 
-    Write-Verbose "Getting Windows Subsystem For Linux (wsl) Version"
-    $(wsl --version) |
-    Out-String |
-    Write-Host
+    if (Get-Command -Name "wsl" -ErrorAction SilentlyContinue) {
+        Write-Verbose "Getting Windows Subsystem For Linux (wsl) Version"
+        $(wsl --version) |
+        Out-String |
+        Write-Host
+    }
+    else {
+        Write-Warning "Windows Subsystem For Linux (wsl) Not Found"
+    }
 }
 end {
     Write-Verbose "Ending $($MyInvocation.MyCommand.Name)"
