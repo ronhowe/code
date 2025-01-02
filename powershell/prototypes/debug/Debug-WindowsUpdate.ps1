243a54b9c54078b7d5039eb3285129e6a2b902c4
Ron Howe <ronhowe@hotmail.com>
2025-01-02 07:18:26 -0500

    Updated Debug Windows Update Script

--- powershell/prototypes/debug/Debug-WindowsUpdate.ps1
+++ powershell/prototypes/debug/Debug-WindowsUpdate.ps1
@@ -12,10 +12,7 @@ Find-Module -Name "PSWindowsUpdate" -Repository "PSGallery"
 # -------    ----                                ----------           -----------
 # 2.2.1.5    PSWindowsUpdate                     PSGallery            This module contain cmdlets to manage Windows Update Client.
 
+## NOTE: This module does not support Invoke-Command due to weirdness of Windows Update.  Commands must be run locally.
 Install-Module -Name "PSWindowsUpdate" -Repository "PSGallery" -Force
 Import-Module -Name "PSWindowsUpdate"
-Get-WindowsUpdate -Install -AcceptAll -AutoReboot -Verbose
-
-Get-Command -Module "PSWindowsUpdate"
-
-Get-WindowsUpdate -Verbose
+Get-WindowsUpdate -Install -AcceptAll -AutoReboot
