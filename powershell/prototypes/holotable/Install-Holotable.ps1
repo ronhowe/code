e7d17512f7363dcb8b09decaa62be3c10d66c6cd
Ron Howe <ronhowe@hotmail.com>
2025-01-27 16:32:48 -0500

    Fixed Holotable Script

--- powershell/prototypes/holotable/Install-Holotable.ps1
+++ powershell/prototypes/holotable/Install-Holotable.ps1
@@ -28,13 +28,13 @@ process {
     $ErrorActionPreference = "Stop"
 
     Write-Verbose "Copying Card Definitions"
-    Copy-Item -Path (Join-Path -Path $Path -ChildPath "*.cdf") -Destination $TargetCdfPath -Force -PassThru
+    Copy-Item -Path (Join-Path -Path $SourceCdfPath -ChildPath "*.cdf") -Destination $TargetCdfPath -Force -PassThru
 
     ## NOTE: Only needed if file hashes are incorrect.
     Write-Verbose "Asserting Include Images Switch"
     if ($IncludeImages) {
         Write-Verbose "Copying Card Images"
-        Copy-Item -Path (Join-Path -Path $Path -ChildPath "Images-HT\starwars") -Destination (Join-Path -Path $TargetCdfPath -ChildPath "cards") -Recurse -Force -PassThru
+        Copy-Item -Path (Join-Path -Path $SourceCdfPaths -ChildPath "Images-HT\starwars") -Destination (Join-Path -Path $TargetCdfPath -ChildPath "cards") -Recurse -Force -PassThru
     }
 }
 end {
