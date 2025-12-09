9bb4f616533a31c69dfd30a516d969b2aa7ddc65
Ron Howe <ronhowe@hotmail.com>
2025-12-09 09:57:32 -0500

    Added .NET Code Base

---
+++ dotnet/MyTestProject10/MockLoggerExtensions.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace MyTestProject10;
+
+internal static class MockLoggerExtensions
+{
+    internal static Mock<ILogger<T>> VerifyLogMessage<T>(this Mock<ILogger<T>> logger, string expectedMessage, LogLevel expectedLogLevel)
+    {
+        ArgumentNullException.ThrowIfNull(expectedMessage);
+
+        Func<object, Type, bool> state = (v, t) => v?.ToString()?.CompareTo(expectedMessage) == 0;
+
+#pragma warning disable CA1873 // Avoid potentially expensive logging
+        logger.Verify(
+            x => x.Log(
+                It.Is<LogLevel>(l => l == expectedLogLevel),
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => state(v, t)),
+                It.IsAny<Exception>(),
+                It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)));
+#pragma warning restore CA1873 // Avoid potentially expensive logging
+
+        return logger;
+    }
+}
